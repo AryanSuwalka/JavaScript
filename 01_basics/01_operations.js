d57eb29254423a4046740eec5e2cb8af91694399
Aryan Suwalka <aryansuwalka302@gmail.com>
2026-04-14 12:11:21 +0000

    learning

---
+++ 01_basics/01_operations.js
@@ -0,0 +1,16 @@
+console.log("1" + 2 + 3);
+console.log(1 + 2 + "3");
+console.log("---------------");
+
+console.log( "4" > 9);
+console.log( "004" > 3);
+console.log("---------------");
+
+console.log( null > 0);
+console.log( null >= 0);
+console.log( null == 0);
+
+
+console.log("---------------");
+console.log( "2" == 2);
+console.log( "2" === 2);
--- 01_basics/testTwo.js
+++ 01_basics/testTwo.js
@@ -1,1 +1,33 @@
 console.log("aryan suwalka testing")
+
+
+let score = "33abc";
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score);
+console.log(typeof (valueInNumber));
+console.log((valueInNumber)); // NaN aayega iska Output
+
+
+let scoretwo = null;
+console.log(typeof scoretwo); // object
+
+let valueInNumbertwo = Number(scoretwo);
+console.log(typeof (valueInNumbertwo)); //number
+console.log((valueInNumbertwo)); // 0
+
+
+let testBool = "arsdi";
+let booltestBool  = Boolean (testBool);
+console.log(booltestBool);
+
+
+let newNUm = 343;
+let stringnuber  = String(newNUm);
+console.log(stringnuber);
+console.log(typeof stringnuber);
+
+
+
+
